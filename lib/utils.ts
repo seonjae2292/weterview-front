c589dbd48d8df1c17f923a3623eadb8b9c832efa
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-11-19 14:39:00 +0900

    feat: cookie 관리 utill 추가

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,25 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import { setCookie, getCookie, deleteCookie } from "cookies-next"
 
-export function cn(...inputs: ClassValue[]) {
+const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs))
 }
+
+const getAccessToken = () => {
+  if (!getCookie("accessToken")) {
+    return undefined;
+  }
+
+  return getCookie("accessToken");
+}
+
+const setAccessToken = (accessToken: string) => {
+  setCookie("accessToken", accessToken, { maxAge: 60 * 60, path: "/" });
+}
+
+const deleteAccessToken = () => {
+  deleteCookie("accessToken", { path: "/" });
+}
+
+export { cn, getAccessToken, setAccessToken, deleteAccessToken };
